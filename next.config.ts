59697ffc39cdb53bc205bc07645222fb34211f16
craig <craiglo2@illinois.edu>
2025-06-12 16:58:21 -0500

    defaulted next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "spyfall";
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  images: { unoptimized: true },
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
+  /* config options here */
 };
 
 export default nextConfig;
